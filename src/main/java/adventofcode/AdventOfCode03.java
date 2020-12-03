9642eb41e672c121e417b4921c71e2dae6c37f18
lchuiton <lchuiton@gmail.com>
2020-12-03 12:45:46 -0500

    Day Three - Refactoring
    Use of SimpleImmutableEntry to represent a slope

--- src/main/java/adventofcode/AdventOfCode03.java
+++ src/main/java/adventofcode/AdventOfCode03.java
@@ -1,5 +1,6 @@
 package adventofcode;
 
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.List;
 
 public class AdventOfCode03 {
@@ -10,13 +11,17 @@ public class AdventOfCode03 {
         this.input = input;
     }
 
-    public Long processFirstPart() {
-        Long result = 0L;
-        Integer length = input.get(0).length();
-        Integer position = 0;
+    public Long processFirstPart(SimpleImmutableEntry<Integer, Integer> slope) {
+        int slopeX = slope.getKey();
+        int slopeY = slope.getValue();
+        return treeHitsWithSlope(slopeX, slopeY);
+    }
 
-        Integer slopeX = 3;
-        int slopeY = 1;
+
+    private Long treeHitsWithSlope(int slopeX, int slopeY) {
+        Long result = 0L;
+        int length = input.get(0).length();
+        int position = 0;
 
         for (int verticalIndex = slopeY; verticalIndex < input.size(); verticalIndex = verticalIndex + slopeY) {
             position = position + slopeX;
@@ -31,17 +36,10 @@ public class AdventOfCode03 {
     }
 
 
-    public Long processSecondPart(Integer slopeX, Integer slopeY) {
-        Long result = 0L;
-        Integer length = input.get(0).length();
-        Integer position = 0;
-
-        for (int verticalIndex = slopeY; verticalIndex < input.size(); verticalIndex = verticalIndex + slopeY) {
-            position = position + slopeX;
-
-            if (input.get(verticalIndex).charAt(position % length) == '#') {
-                result++;
-            }
+    public Long processSecondPart(List<SimpleImmutableEntry<Integer, Integer>> slopes) {
+        Long result = 1L;
+        for (SimpleImmutableEntry<Integer, Integer> slope : slopes) {
+            result *= treeHitsWithSlope(slope.getKey(), slope.getValue());
         }
 
         return result;
--- src/test/java/adventofcode/AdventOfCode03Test.java
+++ src/test/java/adventofcode/AdventOfCode03Test.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,7 +39,9 @@ class AdventOfCode03Test {
     void part1_final() {
         code03 = new AdventOfCode03(input);
 
-        Long result = code03.processFirstPart();
+        SimpleImmutableEntry<Integer, Integer> slope31 = new SimpleImmutableEntry<>(3, 1);
+
+        Long result = code03.processFirstPart(slope31);
 
         assertEquals(262L, result);
 
@@ -48,13 +52,15 @@ class AdventOfCode03Test {
     void part2_final() {
         code03 = new AdventOfCode03(input);
 
-        Long result1 = code03.processSecondPart(1, 1);
-        Long result2 = code03.processSecondPart(3, 1);
-        Long result3 = code03.processSecondPart(5, 1);
-        Long result4 = code03.processSecondPart(7, 1);
-        Long result5 = code03.processSecondPart(1, 2);
+        SimpleImmutableEntry<Integer, Integer> slope1 = new SimpleImmutableEntry<>(1, 1);
+        SimpleImmutableEntry<Integer, Integer> slope2 = new SimpleImmutableEntry<>(3, 1);
+        SimpleImmutableEntry<Integer, Integer> slope3 = new SimpleImmutableEntry<>(5, 1);
+        SimpleImmutableEntry<Integer, Integer> slope4 = new SimpleImmutableEntry<>(7, 1);
+        SimpleImmutableEntry<Integer, Integer> slope5 = new SimpleImmutableEntry<>(1, 2);
+        List<SimpleImmutableEntry<Integer, Integer>> listOfValues = Arrays.asList(slope1, slope2, slope3, slope4, slope5);
 
-        Long result = result1 * result2 * result3 * result4 * result5;
+        Long result = code03.processSecondPart(listOfValues);
+
         assertEquals(2698900776L, result);
 
     }
